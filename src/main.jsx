23369f9d549f2211b73f9c7ca9164b72de39034c
anadelalrashed2008-wq <anadelalrashed2008@gmail.com>
2026-07-03 20:34:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GCCDashboard from "./GCCDashboard.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GCCDashboard />
+  </React.StrictMode>
+);
